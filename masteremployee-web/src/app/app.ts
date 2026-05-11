6cb2f4857f41877e999a9110f6361df722241d35
Edward-campbell <edward@between.as>
2026-05-11 13:24:25 +0200

    init

--- masteremployee-web/src/app/app.ts
+++ masteremployee-web/src/app/app.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/cor
 import { HeaderComponent } from './components/header.component';
 import { TestimonialsComponent } from './components/testimonials.component';
 import { ContactFormComponent } from './components/contact-form.component';
+import { RevealDirective } from './reveal.directive';
 import { I18nService } from './i18n/i18n.service';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TestimonialsComponent, ContactFormComponent],
+  imports: [HeaderComponent, TestimonialsComponent, ContactFormComponent, RevealDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
